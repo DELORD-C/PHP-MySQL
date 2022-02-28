88b8e482b68f9f5ee4151214ac4845b099016f74
Clément DELORD <cdelord@dawan.fr>
2022-02-28 16:13:43 +0100

    date hours

--- Jour 1/dates.php
+++ Jour 1/dates.php
@@ -1,9 +1,12 @@
 <?php
 
-function frDate () {
+function frDate (bool $withHours = false) {
     setlocale(LC_TIME, "fr_FR", "French");
     $date = utf8_encode(strftime('%A %d %B'));
     $exp = explode(' ', $date);
+    if ($withHours) {
+        $exp[2] .= ' ' . date('H:i');
+    }
     return ucfirst($exp[0]) . ' ' . $exp[1] . ' ' . ucfirst($exp[2]);
 }
 
