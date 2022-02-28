2ef0ed2a5524303ca1970eb772bba2e61d238b01
Clément DELORD <cdelord@dawan.fr>
2022-02-28 16:34:42 +0100

    files

--- Jour 1/form.php
+++ Jour 1/form.php
@@ -10,12 +10,21 @@
     <form action="" method='POST' enctype="multipart/form-data">
         <input type="text" name="id">
         <input type="password" name="pass">
-        <!-- <input type="file" name='file'> -->
+        <input type="file" name='file'>
         <input type="submit" value='Envoyer'>
     </form>
     <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     if ($_POST['id'] == 'root' && $_POST['pass'] == 'root') {
         echo '<img src="img.png">';
+    }
+
+    if (!empty($_FILES)) {
+        $dir = '/upload/';
+        $newFile = $dir . $_FILES['file']['name'];
+        move_uploaded_file($_FILES['file']['tmp_name'], $newFile);
     }
     ?>
 </body>
