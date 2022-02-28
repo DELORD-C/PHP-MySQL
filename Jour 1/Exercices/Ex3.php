de3ebe7bb85dd19ed2e55c33c5ab590bb4671426
Clément DELORD <cdelord@dawan.fr>
2022-02-28 13:41:13 +0100

    ex 3

---
+++ Jour 1/Exercices/Ex3.php
@@ -0,0 +1,13 @@
+Créer un formulaire contenant les champs suivants :
+    - Nom (type texte)
+    - Prénom  (type texte)
+    - Email (type texte)
+    - Message (type textarea)
+    - Envoyer (type submit)
+
+Lorsque l'on envoi le formulaire, un email doit être envoyé avec les informations suivantes :
+(avec la fonction mail(), simuler un envoi en local)
+
+Destinataire : [email]
+Sujet : Demande de contact [nom] [prenom]
+Message : [message]
