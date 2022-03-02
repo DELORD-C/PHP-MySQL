c48d6c9adc5778f47a4fa29758a4a36be65ae9cf
Clément DELORD <cdelord@dawan.fr>
2022-03-02 17:04:08 +0100

    coments

--- Jour 3/MyCMS/display.php
+++ Jour 3/MyCMS/display.php
@@ -6,14 +6,20 @@ $requete->execute(['id' => $_GET['page']]);
 $page = $requete->fetch();
 
 $regex = "";
+
+// preg match all sert à rechercher toutes les occurence d'une regex (1er parametre)
+// dans une chaine de caractères (2e paramètre) et stocker les résultats dans un tableau (3e parametre)
 preg_match_all('/{bloc:([0-9]+)}/', $page['contenu'], $resultats);
 
 if (!empty($resultats)) {
     foreach ($resultats[0] as $index => $blocCode) {
+        // on récupère l'id dans le deuxième tableau
         $id = $resultats[1][$index];
         $requete = $bdd->prepare("SELECT contenu FROM bloc WHERE id = :id");
         $requete->execute(['id' => $id]);
         $bloc = $requete->fetch();
+
+        //on remplace le Code du bloc par le contenu du bloc correspondant
         $page['contenu'] = str_replace($blocCode, $bloc['contenu'], $page['contenu']);
     }
 }
@@ -29,7 +35,6 @@ if (!empty($resultats)) {
     <title><?=$page['nom']?></title>
 </head>
 <body>
-
     <?=$page['contenu']?>
 </body>
 </html>
